a6d7dfdeaef702673194427631d41b10ceb47cff
griffongX <vargahhy@gmail.com>
2026-03-03 20:28:51 +0100

    Ajout classe Reservation et StatutReservation et modif main

--- src/Main.java
+++ src/Main.java
@@ -12,5 +12,20 @@ public class Main {
                 LocalDateTime.of(2026, 3, 10, 18,0), 10);
         Seance yoga = new Seance(2, "Yoga",
                 LocalDateTime.of(2026, 3, 17, 19, 0), 20);
+
+        //Création résa
+        Reservation r1 = new Reservation(bodyPump);
+        r1.ajouterPrestation(sauna);
+        r1.ajouterPrestation(coach);
+
+        //Affichage
+        System.out.println("Résaervation :");
+        System.out.println(r1);
+        System.out.println("Cout prestations =" +r1.coutPrestation() + "euros");
+
+        //Annulation
+        r1.annuler();
+        System.out.println("Après annlation :");
+        System.out.println("Statut = " + r1.getStatut());
     }
 }
